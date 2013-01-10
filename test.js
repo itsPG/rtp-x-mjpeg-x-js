45f5ad71201cb778e04ea82fe9f62b0ed9c1e3ba
PG <smartPG@gmail.com>
2013-01-10 20:58:20 +0800

    delete some files

--- test.js
+++
@@ -1,9 +0,0 @@
-a = require("./mjpeg.js");
-//a.load("movie.mjpeg");
-var list = [];
-list.push("./img/001.jpg");
-list.push("./img/002.jpg");
-list.push("./img/003.jpg");
-//a.create("PG.mjpeg", list);
-
-a.load("PG.mjpeg", true);
